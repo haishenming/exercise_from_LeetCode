b602ee70a16bcb833ce27e41f2c6eb137b27a3ad
xiang.yufan <haishenmingx@gmail.com>
2019-10-23 20:11:05 +0800

    n叉树

---
+++ test/ttt.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        a = 0
+        for i in coordinates:
+            b = i[0] / i[1]
+            if a == 0:
+                a = b
+
+            if a != b:
+                return False
