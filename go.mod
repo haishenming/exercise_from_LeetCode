58776862935dbc4c7ed6aa3a9c15d995d9bc45d2
haishenmingx@gmail.com <XouF]]t^Dz+hJg1)uu3w>
2019-11-16 12:22:32 +0800

    102

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/haishenming/exercise_from_LeetCode
+
+go 1.13
--- test/ttt.py
+++ test/ttt.py
@@ -1,12 +1,37 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 class Solution:
-    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        a = 0
-        for i in coordinates:
-            b = i[0] / i[1]
-            if a == 0:
-                a = b
+    def maxDepth(self, root: TreeNode) -> int:
+        if root == None:
+            return 0
 
-            if a != b:
-                return False
+        left = self.maxDepth(root.left) + 1
+        right = self.maxDepth(root.right) + 1
+
+        return left if left > right else right
 
 
+if __name__ == '__main__':
+    root = TreeNode(3)
+    t9 = TreeNode(9)
+    t20 = TreeNode(20)
+    t15 = TreeNode(15)
+    t7 = TreeNode(7)
+    t91 = TreeNode(91)
+    t92 = TreeNode(92)
+
+    root.right = t9
+    root.left = t20
+    t20.right = t15
+    t20.left = t7
+    t9.left = t91
+    t9.right = t92
+
+    s = Solution()
+
+    print(s.maxDepth(root))
